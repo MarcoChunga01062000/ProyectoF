d31ff6ae47963f63ac2b3360c2478ce4b3849b3b
kevinsoras <kevinsoras@upeu.edu.pe>
2018-11-12 15:14:00 -0500

    s
    s

--- src/main/java/com/sisveco/test/Test.java
+++ src/main/java/com/sisveco/test/Test.java
@@ -9,11 +9,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		UsuarioDao ud = new UsuarioDaoImp();
-		HashMap<String, Object> data = new HashMap<>();
-        data = ud.valdar("marco", "123");
-        System.out.println(data);
-        System.out.println(data.size()); 
+		
 	}
 
 }
