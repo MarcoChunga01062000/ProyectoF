ef9aa4bb48cfa67bc6917139e43044e3bf17d347
mijares123 <38017984+mijares123@users.noreply.github.com>
2018-11-12 14:50:00 -0500

    cambio mijares

--- src/main/java/com/sisveco/daoimp/ClienteDaoImp.java
+++ src/main/java/com/sisveco/daoimp/ClienteDaoImp.java
@@ -1,5 +1,46 @@
 package com.sisveco.daoimp;
 
-public class ClienteDaoImp {
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.sisveco.dao.ClienteDao;
+import com.sisveco.entity.Cliente;
+public class ClienteDaoImp implements ClienteDao{
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	@Override
+	public int create(Cliente u) {
+		// TODO Auto-generated method stub
+		String sql = "INSERT INTO usuario(idusuario,nom_user,clave,estado) values(null,?,?,?)";
+		return jdbcTemplate.update(sql);
+	}
+
+	@Override
+	public int update(Cliente u) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(int key) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Cliente read(int key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Map<String, Object>> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
